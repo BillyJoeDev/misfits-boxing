d2d8c83beff131d0121a2980cf354c7fc6d00927
Travis <arma3cloud@gmail.com>
2022-08-11 07:38:29 -0700

    Updates

--- backend/index.js
+++ backend/index.js
@@ -20,4 +20,4 @@ setupRouter(app);
 // Connect to DB and Start Server
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB_CONNECTION, ()=> console.log('Connected to DB!'));
-app.listen(5000, () => { console.log("Backend has been started on port 5000.");})
+app.listen(process.env.PORT || 5000, () => { console.log("Backend has been started on port 5000.");})
